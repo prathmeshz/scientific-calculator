4001049b6f8a3d0d67204a303364cb83f010b199
sufeiiz <szhao2@gmail.com>
2015-05-05 21:20:12 -0400

    initial commit

---
+++ ScientificCalculator/app/src/main/java/nyc/c4q/sufeiiz/scientificcalculator/ParseInput.java
@@ -0,0 +1,13 @@
+package nyc.c4q.sufeiiz.scientificcalculator;
+
+/**
+ * Created by sufeizhao on 5/5/15.
+ */
+public class ParseInput {
+
+
+    public long doMath(){
+        return 0;
+    }
+}
+
